41a5649cbcfccd7b81fcc7c79121e3dbb0da6c04
鲁浩 <416005376@qq.com>
2016-07-26 11:16:54 +0800

    Try to add search view.

---
+++ LaB-iOS/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  LaB-iOS
+//
+//  Created by 鲁浩 on 16/7/21.
+//  Copyright © 2016年 鲁浩. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController {
+     
+}
