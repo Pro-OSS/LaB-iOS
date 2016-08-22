d72efeaa54cb02f326146d79a0e5a7483f743676
鲁浩 <416005376@qq.com>
2016-08-22 12:36:09 +0800

    Add poster xib.

---
+++ LaB-iOS/UI/Home/Cells/Poster.swift
@@ -0,0 +1,24 @@
+//
+//  Poster.swift
+//  LaB-iOS
+//
+//  Created by 鲁浩 on 16/8/22.
+//  Copyright © 2016年 鲁浩. All rights reserved.
+//
+
+import UIKit
+
+class Poster: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
