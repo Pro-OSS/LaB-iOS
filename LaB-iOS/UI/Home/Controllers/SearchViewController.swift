058fc3192013a8b4d21c0917e53b2997544ba7dd
鲁浩 <416005376@qq.com>
2016-09-04 00:24:09 +0800

    Fix home search.

---
+++ LaB-iOS/UI/Home/Controllers/SearchViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SearchViewController.swift
+//  LaB-iOS
+//
+//  Created by 鲁浩 on 16/9/4.
+//  Copyright © 2016年 鲁浩. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
