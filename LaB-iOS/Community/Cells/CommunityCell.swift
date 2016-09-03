5c890eb01093a5bfea144b0c3a2de39d406dd624
鲁浩 <416005376@qq.com>
2016-09-03 13:09:26 +0800

    Fix UI.

---
+++ LaB-iOS/Community/Cells/CommunityCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommunityCell.swift
+//  LaB-iOS
+//
+//  Created by 鲁浩 on 16/9/3.
+//  Copyright © 2016年 鲁浩. All rights reserved.
+//
+
+import UIKit
+
+class CommunityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
